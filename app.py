1cc0aae217df73acc5106e5341be08498c2025ef
Sujeet <64351924+sujeet111@users.noreply.github.com>
2021-04-17 21:55:47 +0530

    api project

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import requests
 import configparser
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def weather_deshboard():
-  return "Hello World"
+  return render_template('home.html')
 
-@app.route('/results')
+@app.route('/results', methods =['POST'])
 def render_results():
-  return "Results Page"
+    zip_code = request.form['ZipCode']
 
-if __name__ == '__main__':
-  app.run()
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, api_key)
+    temp = "{0:.2f}".format(data['main']["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
 
-
+    return render_template('results.html',location=location, temp=temp, weather=weather, feels_like=feels_like)
 
 def get_api_key():
   config = configparser.ConfigParser()
@@ -24,8 +28,10 @@ def get_api_key():
 
 
 def get_weather_results(zip_code, api_key):
-  api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}".format(zip_code, api_key)
+  api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=metric&appid={}".format(zip_code, api_key)
   r = requests.get(api_url)
   return r.json()
 
-print(get_weather_results("94040",get_api_key()))
+
+if __name__ == '__main__':
+  app.run()
